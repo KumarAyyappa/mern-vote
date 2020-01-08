5664a5bac9d2f93bfcfc50e98b3412ca299f6f1e
ayyappa <kayyapparkv@gmail.com>
2020-01-08 18:44:01 +0530

    connected client to api

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
+import api from '../services/api';
 
-const App=()=><div>APP WORKS!!</div>;
+// const App=()=><div>APP WORKS!!</div>;
+
+class App extends Component{
+    async componentDidMount(){
+        const result=await api.call('post','auth/login',{
+            username:"spknbhdvjb@sgswmsail.com",
+	        password:"984873232169"
+        });
+        console.log(result);
+    }
+
+    render(){
+        return <div>App works</div>;
+    }
+}
 
 export default App;
