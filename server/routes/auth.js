afe5e80aefb836ed6bc24c15985fc5d8cda69ddc
ayyappa <kayyapparkv@gmail.com>
2020-02-06 18:09:27 +0530

    changed for nothing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const handle = require('../handlers');
+const handle =require('../handlers');
 
 router.post('/register', handle.register);
 
